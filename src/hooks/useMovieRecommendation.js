0fb8476cb594c203330039285e5cef4461948d0b
Nicolas Lukestik <nicolaslukestik@gmail.com>
2021-11-14 12:29:13 -0300

    Create hook that return a random movie from top rated movies json

---
+++ src/hooks/useMovieRecommendation.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import { getTopRatedMovies } from "../api/tmdbApi"
+
+export function useMovieRecommendation() {
+	const [ topRatedMovie, setTopRatedMovie ] = useState([])
+	const [ loading, setLoading ] = useState(true)
+	const random = Math.floor(Math.random() * 20)
+
+	async function getRecommendation() {
+
+		try {
+			const movies = await getTopRatedMovies()
+			setTopRatedMovie(movies[random])
+		} 
+		catch (err) {
+			alert(err)
+		}
+		finally{
+			setLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		getRecommendation()
+	})
+
+	return {topRatedMovie, loading}
+}
